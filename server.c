e52d467ebb60e12b09059d483632fa58ec07bf68
Lisiane Betoni <66182691+Lisi3301@users.noreply.github.com>
2020-06-19 08:46:55 -0300

    Create server.c

---
+++ server.c
@@ -0,0 +1,75 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h> /* strlen */
+#include <sys/socket.h>
+#include <arpa/inet.h> /* inet_addr */
+#include <unistd.h> /* close */
+
+int main(int argc, char *argv[])
+{
+    int socket_desc, c, new_socket;
+    struct sockaddr_in server, client;
+    char *message, client_reply[2000];
+
+    /* abre um socket */
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
+    if (socket_desc == -1)
+    {
+        printf("Infelizmente não foi possível criar o socket\n");
+        return 1;
+    }
+
+    /* onde quero ouvir ? */
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY; /* escute em qualquer IP da máquina */
+    server.sin_port = htons(8888);
+
+    /* vincula o socket à porta e ao endereço informados */
+    if (bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0)
+    {
+        printf("Ocorreu um erro ao fazer o bind\n");
+        return 1;
+    }
+
+    printf("Bind efetuado!\n");
+
+    /* escutar */
+    listen(socket_desc, 3);
+
+    /* aceitar conexões entrantes */
+    printf("Aguardando conexões...\n");
+    c = sizeof(struct sockaddr_in);
+    while ((new_socket = accept(socket_desc, (struct sockaddr *) &client, (socklen_t*) &c)))
+    {
+        char *client_ip = inet_ntoa(client.sin_addr);
+        int client_port = ntohs(client.sin_port);
+
+        printf("conexão aceita do client %s:%d\n", client_ip, client_port);
+
+        do {
+            bzero(client_reply, sizeof(client_reply)); /* limpa a variável char[] */
+            /* recebe dados do cliente */
+            if (recv(new_socket, client_reply, 2000, 0) < 0)
+            {
+                printf("Falha no recv\n");
+                return 1;
+            }
+            printf("Resposta recebida.\n");
+            printf("%s\n", client_reply);
+            
+            
+
+            /* resposta ao cliente */
+            message = "Olá Cliente! Conexão recebida com sucesso!";
+            write(new_socket, message, strlen(message));
+        } while(strcmp(client_reply, "exit") != 0);
+    }
+    if (new_socket < 0) 
+    {
+        printf("Infelizmente sua conexão não foi aceita!\n");
+        return 1;
+    }
+
+    return 0;
+}
+
